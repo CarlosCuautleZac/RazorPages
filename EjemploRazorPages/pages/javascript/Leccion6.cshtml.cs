6d63a91427b99dbaf781982dd692bf4b0bd6ffdf
CarlosCuautleZac <191g0235@rcarbonifera.tecnm.mx>
2023-09-06 18:47:47 -0600

    Leccion 6

---
+++ EjemploRazorPages/pages/javascript/Leccion6.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EjemploRazorPages.pages.javascript
+{
+    public class Leccion6Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
