3024ef4001fbfc48693da7a74a4d5b858c76a068
CarlosCuautleZac <191g0235@rcarbonifera.tecnm.mx>
2023-09-28 18:45:57 -0600

    creacion de rows de la tabla al darle click

--- Unidad2/wwwroot/scripts/panaderia.js
+++ Unidad2/wwwroot/scripts/panaderia.js
@@ -2,6 +2,7 @@
 
 var listaProductos;
 const botonera = document.querySelector(".panes div");
+const tabla = document.querySelector(".tabla tbody")
 
 async function descargarProductos() {
     let response = await fetch("https://integracion.itesrc.net/api/panes");
@@ -27,8 +28,37 @@ function crearBotones() {
 }
 
 botonera.addEventListener("click", function (e) {
-    
-});
+    const item = e.target.closest("button");
+    if (item) {
+        const id = item.dataset.id;
+
+        var pan = listaProductos.find(x => x.id == id);
+        if (pan) {
+
+            //para encontrar la row con el dataset que contiene el id hay de dos maneras
+            //manera 1
+            //let row = tabla.childNodes.find(x => x.dataset.id == id);
+            //manera 2
+            let row = tabla.querySelector(`[data-id="${id}"]`)
+
+            if (row) {//si lo encontro
+
+                let cantidad = parseInt(row.cells[1].textContent) + 1;
+                row.cells[1].textContent = cantidad;
+                let subtotal = cantidad * pan.precio;
+                row.cells[3].textContent = "$" + subtotal.toFixed(2);
+            }
+            else {//si no lo encuntre
+                row = tabla.insertRow();
+                row.dataset.id = pan.id;
+                row.insertCell().textContent = pan.descripcion;
+                row.insertCell().textContent = 1;
+                row.insertCell().textContent = pan.precio.toFixed(2);
+                row.insertCell().textContent = pan.precio.toFixed(2);
+            }
+        }
+    }  
+},true);
 
 descargarProductos();
 
