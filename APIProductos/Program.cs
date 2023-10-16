e097a155b1f480259a127764b8bba3543aee24f3
StigmaQueen <171g0218@rcarbonifera.tecnm.mx>
2023-10-16 17:11:42 -0600

    add controllers

--- APIProductos/Program.cs
+++ APIProductos/Program.cs
@@ -4,15 +4,17 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
 builder.Services.AddDbContext<ItesrcneIntegracionContext>(optionsBuilder =>
 
 optionsBuilder.UseMySql("server=204.93.216.11;database=itesrcne_integracion;user=itesrcne_integra;password=integra1"
 , Microsoft.EntityFrameworkCore.ServerVersion.Parse("10.3.29-mariadb")));
 
-builder.Services.AddSignalR();
 builder.Services.AddTransient<CategoriasRepository>();
+builder.Services.AddSignalR();
+
 var app = builder.Build();
 
-app.MapControllers();
 app.UseRouting();
+app.MapControllers();
 app.Run();
